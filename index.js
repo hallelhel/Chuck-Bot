0c5f892b5e8565a164648be17effda68f790ae57
HALLE <hallel14193@gmail.com>
2024-01-07 01:28:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,155 @@
+const TelegramBot = require('node-telegram-bot-api');
+const JOKES_URL = "https://parade.com/968666/parade/chuck-norris-jokes/"
+const puppeteer = require('puppeteer');
+const cheerio = require("cheerio");
+const axios = require("axios");
+const request = require('request');
+const ISO6391 = require("iso-639-1")
+
+const TextTranslationClient = require("@azure-rest/ai-translation-text").default
+const { v4: uuidv4 } = require('uuid');
+
+require("dotenv").config();
+const token = process.env.TOKEN
+const key = process.env.KEY
+const endpoint = process.env.ENDPOINT
+const location = process.env.LOCATION
+
+const bot = new TelegramBot(token, {polling: true});
+
+const translateText = async (text, languageCode) => {
+    const translateCredential = {
+        key: key,
+        location,
+      };
+      const translationClient = new TextTranslationClient(endpoint,translateCredential);
+    
+      const inputText = [{ text: text }];
+      const translateResponse = await translationClient.path("/translate").post({
+        body: inputText,
+        queryParameters: {
+          to: languageCode,
+          from: "en",
+        },
+      });
+    
+      const translations = translateResponse.body;
+      console.log(translations[0].translations[0]?.text)
+      return translations[0].translations[0]?.text
+}
+
+const headers = {
+    Accept: "text/html",
+    "Accept-Encoding": "gzip, deflate, br", //indicates the content encoding usually a compression algorithm.
+    "Accept-Language": "en-US,en;q=0.5",
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0",
+  };
+
+// Function to extract jokes from HTML using Cheerio
+const extractJokes = ($) => {
+    const jokes = [];
+  
+    $(".m-detail--body ol li").each((index, elem) => {
+      const joke = $(elem);
+      jokes.push(joke.text());
+    });
+    return jokes;
+  };
+  
+  // Function to fetch jokes from the specified URL
+  const fetchJokes = async () => {
+    try {
+      const response = await axios.get(JOKES_URL, { headers });
+      const html = response.data;
+      const $ = cheerio.load(html);
+  
+      return extractJokes($);
+    } catch (error) {
+      console.error("Error fetching jokes:", error);
+      return [];
+    }
+  };
+const getJoke = async (msg, number) => {
+    let jokes = []
+    try {
+        jokes = await fetchJokes()
+
+        // check valid number
+        if (number < 1 || number > 101) {
+            bot.sendMessage(
+                msg.chat.id,
+                "Please enter a number between 1 - 101"
+            )
+            return
+        }
+
+        const joke = jokes[number-1]
+        if (joke) {
+            const translated = await translateText(joke, newLanguageCode)
+            bot.sendMessage(msg.chat.id, `${number}. ${translated}`)
+
+        }
+        else {
+            bot.sendMessage(
+                msg.chat.id,
+                "couldn't fetch your joke"
+            )
+        }
+    }
+    catch (error) {
+        console.error("Error", error)
+        bot.sendMessage(msg.chat.id, "Error translating")
+    }
+    finally {
+        jokes.length = 0 
+    }
+}
+
+
+// test Hi massage
+
+bot.on('polling_error', (error) => {
+    console.error('Polling error:', error.message);
+    // Handle the error and retry or take appropriate action
+  });
+  
+  bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Hello, welcome!');
+  });
+  
+bot.on('message', (msg) => {
+    var Hi = "hi";
+    if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+        bot.sendMessage(msg.chat.id,"Hello dear user");
+    }
+    
+});
+bot.onText(/^\d+$/, async (msg) => {
+    const number = parseInt(msg.text)
+    await getJoke(msg, number)
+})
+
+bot.onText(/set language (.+)/, async (msg, match) => {
+    try{
+        const chatId = msg.chat.id;
+        const newLanguage = match[1].toLowerCase();
+        const isoCode = ISO6391.getCode(newLanguage)
+        if (isoCode) {
+            newLanguageCode = isoCode
+            const newLanguageRes = await translateText(
+                "No Problem",
+                newLanguageCode
+            )
+            bot.sendMessage(msg.chat.id, newLanguageRes)
+        }
+        else {
+            bot.sendMessage(msg.chat.id, "please choose valid language")
+        }
+    }
+    catch (error) {
+        console.error("Error:", error)
+        bot.sendMessage(msg.chat.id, "An error occurred, try again")
+    }
+})
